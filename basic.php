2fd24740b8de66293a3089c10ee8652b62424dde
HannesCho <hoonsungcho11@gmail.com>
2022-09-05 14:55:28 +0200

    basic php

--- basic.php
+++ basic.php
@@ -16,9 +16,36 @@ echo "<h2>Simple Calculator<h2><br>";
     <input type="submit">
 </form>    
 <?php 
-$num1 = $_GET["num1"];
-$num2 =  $_GET["num2"];
-echo $num1 + $num2;
+if (isset($num1, $num2)){ 
+    $num1 = $_GET["num1"];
+    $num2 =  $_GET["num2"];
+    echo $num1 + $num2;
+} else {
+    echo "0";
+}
+?>
+
+<?php
+echo "<h2>Mad Libs Game<h2><br>";
+?>
+<form action="basic.php" method="get">
+    Color : <input type="text" name="color">
+    <br>
+    Plural Noun : <input type="text" name="pluralNoun">
+    Celebrity : <input type="text" name="celebrity">
+    <input type="submit">
+</form>    
+<?php 
+if (isset($color, $pluralNoun, $celebrity)){ 
+    $color = $_GET["color"];
+    $pluralNoun = $_GET["pluralNoun"];
+    $celebrity = $_GET["celebrity"];
+    echo "Roses are $color <br>";
+    echo "$pluralNoun are blue<br>";
+    echo "I love $celebrity<br>";
+} else {
+    echo "";
+}
 ?>
 </body>
 </html>
