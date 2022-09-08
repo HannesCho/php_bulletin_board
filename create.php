871e3d10d8cbabddc752d96f0e99b5de17134d40
HannesCho <hoonsungcho11@gmail.com>
2022-09-08 10:28:41 +0200

    Create article

--- create.php
+++ create.php
@@ -5,6 +5,11 @@
     <title>Create Your Article</title>
 </head>
 <body>
-    Create
+    <form action="process_create.php" method="POST">
+        <p><input type="text" name="title" placeholder="Title"></p>
+        <p><textarea name="contents" placeholder="Write the texts"></textarea></p>
+        <p><input type="txet" name="author" placeholder="Author"></p>
+        <p><input type="submit" value="Create!"></p>
+    </form>
 </body>
 </html>
---
+++ process_create.php
@@ -0,0 +1,28 @@
+<?php
+include_once "connectdb.php";
+try {
+    $table_name = "php_board.topic";
+    $filtered = array(
+        'title' =>mysqli_real_escape_string($conn, $_POST['title']),
+        'contents' =>mysqli_real_escape_string($conn, $_POST['contents']),
+        'author' =>mysqli_real_escape_string($conn, $_POST['author']),
+    );
+    $sql = "INSERT INTO {$table_name}
+            (title, contents, created, author)
+            VALUES(
+                '{$filtered['title']}',
+                '{$filtered['contents']}',
+                NOW(),
+                '{$filtered['author']}')";
+    $result = mysqli_query($conn, $sql);
+    if($result === false) {
+        echo "Failed!, Somethings Wrong!";
+    } else {
+        echo "You successfully create new article!";
+        echo "<p><a href='index.php'>Go back to Home</a></p>";
+    }
+}
+catch(Exception $e) {
+    echo $e;
+}
+?>
