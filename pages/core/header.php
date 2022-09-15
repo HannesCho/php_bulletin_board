125598cdec3e0147578dd7388c5a964f6e16633c
HannesCho <hoonsungcho11@gmail.com>
2022-09-15 16:06:52 +0200

    set up scss

---
+++ pages/core/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../../styles/style.css">
+    <title>Avo Board</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <nav class="nav-bar">
+                <ul class="nav-list">
+                    <section class="nav-left">
+                        <li><a href="../index.php" class="nav-link home">Home</a></li>
+                    </section>
+                    <section class="nav-right">
+                        <li><a href="../pages/articles.php" class="nav-link">Articles</a></li>
+                        <li><a href="../pages/signup.php" class="nav-link">Sign up</a></li>
+                        <li><a href="../pages/login.php" class="nav-link">Log in</a></li>
+                    </section>
+                </ul>
+            </nav>
+        </header>
